d987fa52249449092b66cd4730bbf3fe0648c59f
unknown <raheelkr2003@gmail.com>
2024-09-20 22:08:32 +0500

    POST

--- server.mjs
+++ server.mjs
@@ -30,3 +30,14 @@ if (method === 'GET' && parsedUrl.pathname === '/api/items') {
     res.statusCode = 200;
     res.end(JSON.stringify({ message: 'GET request - Fetching all items' }));
 }
+else if (method === 'POST' && parsedUrl.pathname === '/api/items') {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        const newItem = JSON.parse(body);
+        res.statusCode = 201;
+        res.end(JSON.stringify({ message: 'POST request - Adding new item', data: newItem }));
+    });
+}
