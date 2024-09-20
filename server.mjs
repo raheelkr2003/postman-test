e66bf2befd33ad4de0a057c895732a025efb19d8
unknown <raheelkr2003@gmail.com>
2024-09-20 21:47:42 +0500

    home

--- server.mjs
+++ server.mjs
@@ -7,3 +7,7 @@ const server = createServer((req, res) => {
     const { method, url } = req;
     const parsedUrl = new URL(url, `http://${req.headers.host}`);
 });
+if (parsedUrl.pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('This is the Home Page.');
+}
